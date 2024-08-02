d0c7ea9cd7c493fdd8624b48251015b8764a16c6
phumebyte <phumelele.dlamini1000@gmail.com>
2024-08-02 13:26:23 +0200

    Finished database setup and basic styling

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+//Database setup
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getDatabase, ref , push} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js"
+
+const appSettings = {
+    databaseURL : "https://realtime-database-709f4-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+// connects our app to firebase
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
 // fetching document elements from html
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
+
+    push(shoppingListInDB)
     
     console.log(inputValue)
 })
