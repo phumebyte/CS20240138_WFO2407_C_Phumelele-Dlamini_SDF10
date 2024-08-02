ed78347fee4f229a7da0b7111ae0bf533b0edc71
phumebyte <phumelele.dlamini1000@gmail.com>
2024-08-02 13:51:26 +0200

    Created and updated shopping list array

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Database setup
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getDatabase, ref , push} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js"
+import { getDatabase, ref , push, onValue} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js"
 
 const appSettings = {
     databaseURL : "https://realtime-database-709f4-default-rtdb.asia-southeast1.firebasedatabase.app/"
@@ -27,10 +27,22 @@ addButtonEl.addEventListener("click", function() {
     
 })
 
-function clearInputFieldEl() {
+onValue(shoppingListInDB, function(snapshot) {
+    let itemsArray = Object.values(snapshot.val()) // converting the items object into an array
+    console.log(itemsArray)  
+    //for loop to iterate over items in array
+    for(let i=0; i<itemsArray.length; i++){
+       //appends items added by appendItemToShoppingListEl to array so that they show up on the list 
+       appendItemToShoppingListEl(itemsArray[i])
+    }
+    
+
+})
+
+function clearInputFieldEl() {  //strictly clears input field when item has been added to list
     inputFieldEl.value = " "
 }
 
-function appendItemToShoppingListEl(itemValue) {
+function appendItemToShoppingListEl(itemValue) {  // adds item values to list
     shoppingListEl.innerText += '<li>${itemValue}</li>'
 }
